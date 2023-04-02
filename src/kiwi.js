e8f0666c8a242c0f5db37be7eee42de87d62bf3e
Alex <yixiaozhouchen@hotmail.com>
2023-04-02 10:49:39 -0500

    add

--- src/kiwi.js
+++ src/kiwi.js
@@ -1,6 +1,5 @@
 import Heading from './components/heading/heading.js';
 import KiwiImage from './components/kiwi-Image/kiwi-image';
-import React from 'react';
 
 const heading = new Heading();
 heading.render('kiwi');
